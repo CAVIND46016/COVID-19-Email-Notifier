e94f496fff1e12eb9cce5f34d59a99d52a148093
Cavin Dsouza <21344198+CAVIND46016@users.noreply.github.com>
2020-06-13 16:39:15 -0400

    Create email.py

---
+++ settings/email.py
@@ -0,0 +1,11 @@
+"""
+EMAIL connection parameters
+"""
+
+EMAIL_CONNECTION_PARAMS = {
+    "host": "smtp.gmail.com",
+    "port": 587,
+    "user": "mnop@gmail.com",
+    "password": "xxxxxxxxxx",
+    "use_tls": True
+}
